d435b92180268e5416f5805c2e5c93e80c09880a
Yuri Almeida <yurialves2@gmail.com>
2022-06-16 21:26:07 -0300

    refactor: istanbul coverage ignore

--- src/sort/insertion-sort.ts
+++ src/sort/insertion-sort.ts
@@ -1,15 +1,17 @@
 import { swapItemsPosition } from '../utils/swap-items-position';
 
-// Insertion sort:
-//   The insertion sort algorithm works by iterating over the array, finding
-//   the correct position for the current element and then swapping it with
-//   the element at that position (INSERTING the element on the correct
-//   position).
+/**
+ *
+ * @description Insertion sort:
+ * The insertion sort algorithm works by iterating over the array, finding
+ * the correct position for the current element and then swapping it with
+ * the element at that position (INSERTING the element on the correct
+ * position).
+ */
 export function insertionSort<T>(list: T[]): T[] {
   for (let currentIndex = 1; currentIndex < list.length; currentIndex++) {
     let analyzedItemIndex = currentIndex;
     let previousItemIndex = analyzedItemIndex - 1;
-    if (analyzedItemIndex === 0) continue;
     while (list[analyzedItemIndex] < list[previousItemIndex]) {
       swapItemsPosition(list, analyzedItemIndex, previousItemIndex);
       analyzedItemIndex--;
@@ -19,10 +21,11 @@ export function insertionSort<T>(list: T[]): T[] {
   return list;
 }
 
+/* istanbul ignore next */
 async function main() {
   const { books } = await import('../data-sources/book-list');
   console.log(insertionSort(books.map((book) => book.price)));
   console.log(insertionSort(books.map((book) => book.title)));
 }
-
+/* istanbul ignore next */
 if (require.main === module) main();
--- src/sort/selection-sort.ts
+++ src/sort/selection-sort.ts
@@ -1,9 +1,12 @@
 import { lowerNumber } from '../utils/lower-number';
 import { swapItemsPosition } from '../utils/swap-items-position';
 
-// Selection sort:
-//   The selection sort algorithm works by iterating over the array, finding
-//   the smallest element and swapping it with the current element.
+/**
+ *
+ * @description Selection sort:
+ * The selection sort algorithm works by iterating over the array, finding
+ * the smallest element and swapping it with the current element.
+ */
 export function selectionSort<T>(list: T[]): T[] {
   for (let currentIndex = 0; currentIndex < list.length; currentIndex++) {
     const cheaperBookIndex = lowerNumber(list, currentIndex);
@@ -11,11 +14,11 @@ export function selectionSort<T>(list: T[]): T[] {
   }
   return list;
 }
-
+/* istanbul ignore next */
 async function main() {
   const { books } = await import('../data-sources/book-list');
   const bookPrices = books.map((book) => book.price);
   console.log(selectionSort(bookPrices));
 }
-
+/* istanbul ignore next */
 if (require.main === module) main();
