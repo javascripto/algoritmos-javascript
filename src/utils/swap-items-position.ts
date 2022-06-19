73b43fe89413a52e61be7f97f16bb146c5c8b2f5
Yuri Almeida <yurialves2@gmail.com>
2022-06-19 14:16:01 -0300

    chore: added $swap to Array prototype

--- src/utils/swap-items-position.ts
+++ src/utils/swap-items-position.ts
@@ -2,8 +2,19 @@ export function swapItemsPosition<T>(
   array: T[],
   indexA: number,
   indexB: number
-): void {
+): T[] {
   const temp = array[indexA];
   array[indexA] = array[indexB];
   array[indexB] = temp;
+  return array;
 }
+
+declare global {
+  interface Array<T> {
+    $swap(index1: number, index2: number): T[];
+  }
+}
+
+Array.prototype.$swap = function <T>(index1: number, index2: number): T[] {
+  return swapItemsPosition(this, index1, index2);
+};
