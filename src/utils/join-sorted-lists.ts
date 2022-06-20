85d741d648ab501745a64a0b0c1bcf6325065b3a
Yuri Almeida <yurialves2@gmail.com>
2022-06-20 09:05:40 -0300

    refactor: join sorted lists

--- src/utils/join-sorted-lists.ts
+++ src/utils/join-sorted-lists.ts
@@ -5,27 +5,35 @@ import { ComparatorFn, simpleComparator } from './comparator';
  * @description Join two sorted lists into a new sorted list.
  * It will not work if the lists passed as argument are not sorted.
  */
+
 export function joinSortedLists<T>(
-  firstSortedList: T[],
-  secondSortedList: T[],
+  listA: T[],
+  listB: T[],
   comparatorFn: ComparatorFn<T> = simpleComparator
 ): T[] {
-  const newList = Array(firstSortedList.length + secondSortedList.length);
-  for (let i = 0, a = 0, b = 0; i < newList.length; i++) {
-    const itemA = firstSortedList[a];
-    const itemB = secondSortedList[b];
-    const reachedEndOfListB = itemB === undefined;
-    const itemAIsLessOrEqualToItemB = () => {
-      if (itemA === undefined || itemB === undefined) return false;
-      return comparatorFn(itemA, itemB) < 1;
-    };
-    if (reachedEndOfListB || itemAIsLessOrEqualToItemB()) {
-      newList[i] = itemA;
+  const newList = Array(listA.length + listB.length);
+  let a = 0;
+  let b = 0;
+  let current = 0;
+  while (a < listA.length && b < listB.length) {
+    if (comparatorFn(listA[a], listB[b]) < 0) {
+      newList[current] = listA[a];
       a++;
     } else {
-      newList[i] = itemB;
+      newList[current] = listB[b];
       b++;
     }
+    current++;
+  }
+  while (a < listA.length) {
+    newList[current] = listA[a];
+    a++;
+    current++;
+  }
+  while (b < listB.length) {
+    newList[current] = listB[b];
+    b++;
+    current++;
   }
   return newList;
 }
