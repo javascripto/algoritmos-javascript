83c8d70ed678c91a282779196985ea43217278b6
Yuri Almeida <yurialves2@gmail.com>
2022-06-26 20:52:17 -0300

    chore: added divide on pivot function

--- src/utils/find-smaller-values.ts
+++ src/utils/find-smaller-values.ts
@@ -17,3 +17,28 @@ export function findSmallerValues<T>(
   list.$swap(indexOfPivot, smallerValues);
   return list;
 }
+
+export function divideOnPivot<T>(
+  list: T[],
+  comparatorFn: ComparatorFn<T> = simpleComparator
+): T[] {
+  let pivot = list[Math.floor(list.length / 2)];
+  console.log(pivot);
+  findSmallerValues(pivot, list);
+  let smallerValues = 0;
+  for (let current = 0; current < list.length; current++) {
+    const currentItem = list[current];
+    if (currentItem !== pivot && comparatorFn(currentItem, pivot) < 1) {
+      list.$swap(current, smallerValues);
+      smallerValues++;
+    }
+  }
+  return list;
+}
+
+/* istanbul ignore next */
+if (require.main === module) {
+  const list = [3, 6, 2, 5, 8, 7];
+  console.log(list);
+  console.log(divideOnPivot(list));
+}
