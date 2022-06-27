fc05f96236caec81989a38370fce6939383b1670
Yuri Almeida <yurialves2@gmail.com>
2022-06-26 21:38:46 -0300

    fix: quick sort comparator function

--- src/sort/quick-sort/quick-sort.ts
+++ src/sort/quick-sort/quick-sort.ts
@@ -8,7 +8,7 @@ export function quickSort<T>(
   comparatorFn: ComparatorFn<T> = simpleComparator
 ): T[] {
   if (list.length <= 1) return list;
-  let currentIndex = partition(list, leftIndex, rightIndex);
+  let currentIndex = partition(list, leftIndex, rightIndex, comparatorFn);
   if (leftIndex < currentIndex - 1) {
     quickSort(list, leftIndex, currentIndex - 1, comparatorFn);
   }
@@ -31,7 +31,6 @@ function partition<T>(
     while (comparatorFn(list[currentLeftIndex], pivot) < 0) {
       currentLeftIndex++;
     }
-
     while (comparatorFn(list[currentRightIndex], pivot) > 0) {
       currentRightIndex--;
     }
@@ -50,4 +49,16 @@ if (require.main === module) {
   console.log(list);
   quickSort(list, 0, list.length - 1);
   console.log(list);
+  const people = [
+    { name: 'Joe', age: 40 },
+    { name: 'Jane', age: 30 },
+    { name: 'Jack', age: 50 },
+    { name: 'John', age: 20 },
+  ];
+  quickSort(people, 0, people.length - 1, (a, b) => {
+    if (a.age < b.age) return -1;
+    if (a.age > b.age) return 1;
+    return 0;
+  });
+  console.log(people);
 }
